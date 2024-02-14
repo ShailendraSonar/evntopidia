21b525e4f2ee8d3b01c896204606b72f6037d678
satyendra <msatyendra567@gmail.com>
2024-02-14 13:19:39 +0530

    dashboard styling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,7 +11,7 @@ import Dashboard from './pages/Dashboard';
 function App() {
   return (
     <div className="min-h-full h-screen flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
-    <div className="max-w-md w-full space-y-8">
+    <div className="">
      <BrowserRouter>
         <Routes>
             <Route path="/" element={<LoginPage/>} />
--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Dashboard() {
   return (
-    <div>
+    <div className='bg-slate-900 text-center w-screen h-screen flex justify-center items-center'>
         <h1 className='text-slate-400'>Wellcome to Admin pannel</h1>
     </div>
   )
